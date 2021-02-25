f66954b94b6c4911fd3ab3818503708f1c3f7f3e
Marcelo Tadini <tadini.marcelo53@gmail.com>
2021-02-24 21:38:18 -0300

    Final countdown, Contexts e barra de level

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -1,14 +1,21 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/ExperienceBar.module.css';
 
 export function ExperienceBar () {
+
+    const {currentExperience, experienceToNextLevel} = useContext(ChallengesContext);
+
+    const percentToNextLevel = Math.floor(currentExperience * 100) / experienceToNextLevel;
+
     return(
         <header className={styles.experienceBar}>
             <span>@ xp</span>
             <div>
-                <div style={{width: '50%'}} />
-                <span className={styles.currentExperience} style={{left: '50%'}}>300px</span>
+                <div style={{width: `${percentToNextLevel}%`}} />
+                <span className={styles.currentExperience} style={{left: `${percentToNextLevel}%`}}>{currentExperience} px</span>
             </div>
-            <span>600 xp</span>
+            <span>{experienceToNextLevel} xp</span>
         </header>
     );
 }
