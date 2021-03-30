9b8339b0ee34dd134e445ac1d0b551f57342b145
Marcelo Tadini <tadini.marcelo53@gmail.com>
2021-03-30 15:53:49 -0300

    Alteração para compartilahr com foto e descrição

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,9 +8,9 @@ export default class MyDocument extends Document {
                     <link rel="shortcut icon" href="favicon.png" type="image/png"/>
                     <link rel="preconnect" href="https://fonts.gstatic.com"/>
                     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap" rel="stylesheet"/>
-                    <meta property="og:image" content="https://raw.githubusercontent.com/rocketseat-education/nlw-04-reactjs/main/.github/logo.png" />
+                    <meta property="og:image" content="https://avatars.githubusercontent.com/u/57718626?s=460&u=ddffa661d917bb9445326c75bb7b8916405412c1&v=4" />
                     <meta property="og:title" content="Move It | by Marcelo Tadini"/>
-                    <meta property="og:description" content="Faça pardsadsaadas e alguns exercícios periodicamente enquanto trabalha ou estuda na frente do computador"/>
+                    <meta property="og:description" content="Faça pausas e alguns exercícios periodicamente quando o sinal de alerta tocar enquanto trabalha ou estuda na frente do computador"/>
                 </Head>
                 <body>
                     <Main />
