afa151123fbc4a1b981e8004e24cb4d03a816eab
Marcelo Tadini <tadini.marcelo53@gmail.com>
2021-03-30 15:39:48 -0300

    Alteração para compartilahr com foto e descrição

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,9 @@ export default class MyDocument extends Document {
                     <link rel="shortcut icon" href="favicon.png" type="image/png"/>
                     <link rel="preconnect" href="https://fonts.gstatic.com"/>
                     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap" rel="stylesheet"/>
+                    <meta property="og:image" content="https://raw.githubusercontent.com/rocketseat-education/nlw-04-reactjs/main/.github/logo.png" />
+                    <meta property="og:title" content="Move It | by Marcelo Tadini"/>
+                    <meta property="og:description" content="Faça paradas e alguns exercícios periodicamente enquanto trabalha ou estuda na frente do computador"/>
                 </Head>
                 <body>
                     <Main />
